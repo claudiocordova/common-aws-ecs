9adf152e8227fff5153cc4ec77823824772c785b
Claudio Cordova <claudio_cordova@yahoo.com>
2023-03-31 17:33:47 -0700

    Added EC2 mode

--- create-stack.sh
+++ create-stack.sh
@@ -1,9 +1,31 @@
-region=$(aws configure get region)
+REGION=$(aws configure get region)
+
+if [ -z "$1" ]; then
+      MODE=FARGATE
+elif [ "$1" == "EC2" ]; then
+      MODE=$1
+elif [ "$1" == "FARGATE" ]; then
+      MODE=$1
+else
+    echo "Wrong parameter 1 MODE: "$1
+    exit 1 
+fi
 
 
-aws cloudformation create-stack --region $region --stack-name ecs-vpc-stack --template-body file://./ecs-vpc.yaml --capabilities CAPABILITY_IAM
-aws cloudformation wait stack-create-complete --region $region --stack-name ecs-vpc-stack
-aws cloudformation create-stack --region $region  --stack-name ecs-cluster-stack --template-body file://./ecs-cluster.yaml --capabilities CAPABILITY_IAM
-aws cloudformation wait stack-create-complete --region $region --stack-name ecs-cluster-stack
-aws cloudformation create-stack --region $region  --stack-name ecs-service-stack --template-body file://./ecs-service.yaml --capabilities CAPABILITY_IAM
-aws cloudformation wait stack-create-complete --region $region --stack-name ecs-service-stack
+aws cloudformation create-stack --region $REGION --stack-name ecs-vpc-stack --template-body file://./ecs-vpc.yaml --capabilities CAPABILITY_IAM
+aws cloudformation wait stack-create-complete --region $REGION --stack-name ecs-vpc-stack
+
+if [ "$MODE" == "FARGATE" ]; then
+  aws cloudformation create-stack --region $REGION  --stack-name ecs-cluster-stack --template-body file://./ecs-cluster.yaml --capabilities CAPABILITY_IAM
+elif [ "$MODE" == "EC2" ]; then
+  aws cloudformation create-stack --region $REGION  --stack-name ecs-cluster-stack --template-body file://./ecs-ec2-cluster.yaml --capabilities CAPABILITY_IAM
+fi
+aws cloudformation wait stack-create-complete --region $REGION --stack-name ecs-cluster-stack
+
+if [ "$MODE" == "FARGATE" ]; then
+  aws cloudformation create-stack --region $REGION  --stack-name ecs-service-stack --template-body file://./ecs-service.yaml --capabilities CAPABILITY_IAM
+elif [ "$MODE" == "EC2" ]; then
+  aws cloudformation create-stack --region $REGION  --stack-name ecs-service-stack --template-body file://./ecs-ec2-service-elb-asg.yaml --capabilities CAPABILITY_IAM
+fi
+
+aws cloudformation wait stack-create-complete --region $REGION --stack-name ecs-service-stack
--- delete-stack.sh
+++ delete-stack.sh
@@ -1,10 +1,10 @@
-region=$(aws configure get region)
+REGION=$(aws configure get region)
 
-aws cloudformation delete-stack --region $region --stack-name ecs-service-stack
-aws cloudformation wait stack-delete-complete --region $region --stack-name ecs-service-stack
-aws cloudformation delete-stack --region $region --stack-name ecs-cluster-stack
-aws cloudformation wait stack-delete-complete --region $region --stack-name ecs-cluster-stack
-aws cloudformation delete-stack --region $region --stack-name ecs-vpc-stack
-aws cloudformation wait stack-delete-complete --region $region --stack-name ecs-vpc-stack
+aws cloudformation delete-stack --region $REGION --stack-name ecs-service-stack
+aws cloudformation wait stack-delete-complete --region $REGION --stack-name ecs-service-stack
+aws cloudformation delete-stack --region $REGION --stack-name ecs-cluster-stack
+aws cloudformation wait stack-delete-complete --region $REGION --stack-name ecs-cluster-stack
+aws cloudformation delete-stack --region $REGION --stack-name ecs-vpc-stack
+aws cloudformation wait stack-delete-complete --region $REGION --stack-name ecs-vpc-stack
 
 
