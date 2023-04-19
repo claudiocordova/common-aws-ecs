36ceb2181ef7fd1479e6a73cd4bd8f93c0b0f599
Claudio Cordova <claudio_cordova@yahoo.com>
2023-04-18 22:22:42 -0700

    First

--- delete-stack.sh
+++ delete-stack.sh
@@ -22,13 +22,16 @@ aws cloudformation wait stack-delete-complete --region $REGION --stack-name ecs-
 #aws cloudformation delete-stack --region $REGION --stack-name ecs-vpc-stack
 #aws cloudformation wait stack-delete-complete --region $REGION --stack-name ecs-vpc-stack
 
-#Second time around it succeeds.
-#Deregister Container Instances: Before you can delete a cluster, you must deregister the container instances inside that cluster. For each container instance inside your cluster, follow the procedures in Deregister a Container Instance to deregister it.
-#Alternatively, you can use the following AWS CLI command to deregister your container instances. Be sure to substitute the Region, cluster name, and container instance ID for each container instance that you are deregistering.
+
+#Deregister Container Instances: Before you can delete a cluster, you must deregister the container instances inside that cluster. 
+#For each container instance inside your cluster, follow the procedures in Deregister a Container Instance to deregister it.
+#Alternatively, you can use the following AWS CLI command to deregister your container instances. 
+#Be sure to substitute the Region, cluster name, and container instance ID for each container instance that you are deregistering.
 #aws ecs deregister-container-instance --cluster default --container-instance container_instance_id --region us-west-2 --force
 
 
 
+#Second time around it succeeds.
 aws cloudformation delete-stack --region $REGION --stack-name ecs-cluster-stack
 
 if [ $? -ne 0 ]; then
