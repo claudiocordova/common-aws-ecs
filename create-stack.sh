c4e78595164851dc10322a845fd93da52ed0c131
Claudio Cordova <claudio_cordova@yahoo.com>
2023-04-25 10:14:30 -0700

    First

--- create-stack.sh
+++ create-stack.sh
@@ -3,10 +3,10 @@
 REGION=$(aws configure get region)
 
 if [ -z "$1" ]; then
-      MODE=FARGATE
-elif [ "$1" == "EC2" ]; then
+      MODE=ECS_FARGATE
+elif [ "$1" == "ECS_EC2" ]; then
       MODE=$1
-elif [ "$1" == "FARGATE" ]; then
+elif [ "$1" == "ECS_FARGATE" ]; then
       MODE=$1
 else
     echo "Wrong parameter 1 MODE: "$1
@@ -17,10 +17,10 @@ fi
 #aws cloudformation create-stack --region $REGION --stack-name ecs-vpc-stack --template-body file://./ecs-vpc.yaml --capabilities CAPABILITY_IAM
 #aws cloudformation wait stack-create-complete --region $REGION --stack-name ecs-vpc-stack
 
-if [ "$MODE" == "FARGATE" ]; then
+if [ "$MODE" == "ECS_FARGATE" ]; then
   aws cloudformation create-stack --region $REGION  --stack-name ecs-cluster-stack --template-body file://./ecs-fargate-cluster.yaml --capabilities CAPABILITY_IAM
   result=$?
-elif [ "$MODE" == "EC2" ]; then
+elif [ "$MODE" == "ECS_EC2" ]; then
   aws cloudformation create-stack --region $REGION  --stack-name ecs-cluster-stack --template-body file://./ecs-ec2-cluster.yaml --capabilities CAPABILITY_IAM
   result=$?
 fi
@@ -37,10 +37,10 @@ fi
 
 aws cloudformation wait stack-create-complete --region $REGION --stack-name ecs-cluster-stack
 
-if [ "$MODE" == "FARGATE" ]; then
+if [ "$MODE" == "ECS_FARGATE" ]; then
   aws cloudformation create-stack --region $REGION  --stack-name ecs-service-stack --template-body file://./ecs-fargate-service-elb-aas.yaml --capabilities CAPABILITY_IAM
   result=$? 
-elif [ "$MODE" == "EC2" ]; then
+elif [ "$MODE" == "ECS_EC2" ]; then
   aws cloudformation create-stack --region $REGION  --stack-name ecs-service-stack --template-body file://./ecs-ec2-service-elb-asg.yaml --capabilities CAPABILITY_IAM
   result=$?
 fi
